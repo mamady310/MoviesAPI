6776b48e979ca9ccfef50cc22ad861d0a902dd31
MS <supurpisces310@msn.com>
2024-02-05 09:45:37 -0500

    Added Movies class to handle API requests for movies.

---
+++ src/main/java/com/moviesAPI/moviesAPI/Movies.java
@@ -0,0 +1,26 @@
+package com.moviesAPI.moviesAPI;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.List;
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movies {
+
+    @Id //lets spring know that objecId is unique identifier
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+}
