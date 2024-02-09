1b79f55c9a37d8b14bd606a19a0c8b780c428574
MS <supurpisces310@msn.com>
2024-02-09 12:12:49 -0500

    Added annotation to allow any client to access moviesApi.

--- src/main/java/com/moviesAPI/moviesAPI/ReviewController.java
+++ src/main/java/com/moviesAPI/moviesAPI/ReviewController.java
@@ -3,15 +3,12 @@ package com.moviesAPI.moviesAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
-
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/reviews")
 public class ReviewController {
 
